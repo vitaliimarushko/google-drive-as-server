e4cbc200c28f36bbae2d8c35717cd5617a7819ff
Vitalii Marushko <v.marushko.ossystem@gmail.com>
2021-03-04 09:39:16 +0200

    test csv content

--- js/script.js
+++ js/script.js
@@ -1,30 +1,92 @@
-const byId = id => document.getElementById(id);
+// const byId = id => document.getElementById(id);
 
-const loadContent = async () => {
+const csvToJson = csv => {
+  const lines = csv.split('\n');
+  const result = [];
+  const headers = lines[0].split(',');
+
+  for (let i = 1; i < lines.length - 1; i++) {
+    const obj = {};
+    const currentLine = lines[i].split(',');
+
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentLine[j];
+    }
+
+    result.push(obj);
+  }
+
+  return result;
+};
+
+const makeRequest = async url => {
   let content = null;
-  const contentDataRef = byId('app-content-data');
   const requestParams = {
-    url: 'data/lorem.txt',
-    timeout: 10000,
+    url,
+    timeout: 30000,
     headers: {
       'Cache-Control': 'no-cache',
+      // accept: 'text/plain',
     },
-    responseType: 'text',
+    transformResponse: [data => {
+      // Do whatever you want to transform the data
+      console.log('data', data);
+
+      return data;
+    }],
+    withCredentials: true,
+    maxRedirects: 0,
+    responseType: 'blob',
   };
 
   try {
     const response = await axios(requestParams) || {};
-    content = response?.request?.responseText || '';
+    content = await response.data.text();
   } catch (ex) {
     console.error(ex);
     return false;
   }
 
-  contentDataRef.innerText = content;
+  return content;
 };
 
-const loadedCb = () => {
-  byId('app-get-content-btn').addEventListener('click', loadContent);
+// const loadContent = async () => {
+//   const contentDataRef = byId('app-content-data');
+//   const content = await makeRequest('data/lorem.txt');
+//
+//   contentDataRef.innerText = content;
+// };
+
+const loadedCb = async () => {
+  // byId('app-get-content-btn').addEventListener('click', loadContent);
+
+  const tableRow = document.getElementsByClassName('app-header-row')[0];
+  const content = await makeRequest('data/catalog.csv');
+
+  console.log('content', content);
+
+  // const csv = csvToJson(content);
+  // let htmlContent = '';
+  //
+  // for (const csvElement of csv) {
+  //   const tr = `
+  //     <tr>
+  //       <td>
+  //         ${csvElement.name}
+  //       </td>
+  //       <td>
+  //         ${csvElement.price}
+  //       </td>
+  //       <td>
+  //         ${csvElement.description || '-'}
+  //       </td>
+  //     </tr>
+  //   `;
+  //
+  //   htmlContent += tr;
+  // }
+  //
+  // tableRow.insertAdjacentHTML('afterend', htmlContent);
 };
 
 document.addEventListener('DOMContentLoaded', loadedCb, false);
